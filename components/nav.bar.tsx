4e836ceaa582ae0a21051133dcc250a21682c0ae
lumpenop <swk5373@gmail.com>
2022-10-30 03:54:37 +0900

    add: create next app --ts

---
+++ components/nav.bar.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const NavBar = () => {
+  return (
+    <header>
+      <Head>
+        <title>title</title>
+      </Head>
+      <div style={{ color: "white" }}>hi</div>
+    </header>
+  );
+};
+
+export default NavBar;
