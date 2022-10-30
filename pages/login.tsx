c5b30d605da2a58453b3452462d3d67006e237e4
lumpenop <swk5373@gmail.com>
2022-10-31 05:10:19 +0900

    feat: 회원가입, 로그인 submit 시 api 호출 추가

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,46 +1,62 @@
-import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { useForm, SubmitHandler } from "react-hook-form";
+import Auth from "../api/auth";
+import { AxiosError } from "axios";
 
 type Inputs = {
-  userId: string;
+  email: string;
   password: string;
 };
 
 const Login = () => {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm<Inputs>({ mode: "onChange" });
-  const onChange: SubmitHandler<Inputs> = (data) => console.log(data);
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  } = useForm<Inputs>({ mode: "onChange", shouldFocusError: false });
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    try {
+      Auth.login(data)
+        .then((res) => {
+          const { access_token } = res.data.response;
+          console.log(access_token);
+          if (access_token) return router.push("/board");
+        })
+        .catch((e) => {
+          if (e instanceof AxiosError && e.response) {
+            const { status } = e.response;
+            if (status === 400) console.log("password check");
+            if (status === 404) console.log("not found");
+          }
+        });
+    } catch (e) {
+      throw new Error("알 수 없는 서버 에러");
+    }
+  };
 
   return (
     <section style={{ marginTop: 100 }}>
-      <div style={{ width: "45%", margin: "0 auto" }}>
+      <div style={{ width: "35%", margin: "0 auto" }}>
         <h1>login</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
-          {/* register your input into the hook by invoking the "register" function */}
           <div>
             <label>id </label>
             <input
-              {...register("userId", {
+              {...register("email", {
                 pattern: { value: /^[A-Za-z]+$/i, message: "아이디는 영어만" },
                 required: true,
               })}
             />
-            <span>{errors.userId?.message}</span>
+            <span>{errors.email?.message}</span>
           </div>
-          {/* include validation with required or other standard HTML validation rules */}
           <div>
             <label>pw </label>
             <input
               type={"password"}
               {...register("password", { required: true })}
             />
-            {/* errors will return when field validation fails  */}
-            {errors.password && <span>This field is required</span>}
+            {errors.password && <span>비밀번호를 입렵해주세요</span>}
           </div>
           <input type="submit" />
         </form>
