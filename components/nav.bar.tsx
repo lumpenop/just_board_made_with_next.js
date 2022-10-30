c03554ec7e3ef0769de8fd1ab63166f8101d733c
lumpenop <swk5373@gmail.com>
2022-10-31 00:14:56 +0900

    feat: react-hook-form 추가하여 login 페이지 구성

--- components/nav.bar.tsx
+++ components/nav.bar.tsx
@@ -1,12 +1,44 @@
 import Head from "next/head";
 
+import { useRouter } from "next/router";
+
 const NavBar = () => {
+  const router = useRouter();
   return (
     <header>
       <Head>
         <title>title</title>
       </Head>
-      <div style={{ color: "white" }}>hi</div>
+      <div
+        style={{
+          color: "black",
+          height: "40px",
+          lineHeight: "40px",
+          backgroundColor: "skyblue",
+          padding: "0 40px 0 40px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <span>hi</span>
+        <div>
+          <button
+            type={"button"}
+            style={{ height: 20 }}
+            onClick={() => router.push("login")}
+          >
+            login
+          </button>
+          <button
+            type={"button"}
+            style={{ height: 20, marginLeft: 10 }}
+            onClick={() => router.push("signup")}
+          >
+            sign up
+          </button>
+        </div>
+      </div>
     </header>
   );
 };
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,52 @@
+import { useEffect } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  userId: string;
+  password: string;
+};
+
 const Login = () => {
-  return <div>login</div>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>({ mode: "onChange" });
+  const onChange: SubmitHandler<Inputs> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  return (
+    <section style={{ marginTop: 100 }}>
+      <div style={{ width: "45%", margin: "0 auto" }}>
+        <h1>login</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* register your input into the hook by invoking the "register" function */}
+          <div>
+            <label>id </label>
+            <input
+              {...register("userId", {
+                pattern: { value: /^[A-Za-z]+$/i, message: "아이디는 영어만" },
+                required: true,
+              })}
+            />
+            <span>{errors.userId?.message}</span>
+          </div>
+          {/* include validation with required or other standard HTML validation rules */}
+          <div>
+            <label>pw </label>
+            <input
+              type={"password"}
+              {...register("password", { required: true })}
+            />
+            {/* errors will return when field validation fails  */}
+            {errors.password && <span>This field is required</span>}
+          </div>
+          <input type="submit" />
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
